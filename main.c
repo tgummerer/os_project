44a76875003c03d76f6ccfe03cec802f803c53d9
Thomas Gummerer <t.gummerer@gmail.com>
2010-04-19 12:23:43 +0000

    * Fork structure added

--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (int args, char ** argv)
 {
-  char command [255];
+  char command [256];
+  int pid;
   
-  printf("$ ");
-  scanf("%s", command);
+  // Loop (nearly) forever
+  for (;;) {
+    printf("$ ");
+    // Read a command from the console
+    scanf("%s", command);
 
-  printf("%s\n", command);
+    // exit, if the command is exit
+    if (strcmp ("exit", command) == 0) {
+      break;
+    }
+    else {
+      // fork to execute new program
+      if (pid = fork() < 0) {
+	printf("Fork failed, program exiting\n");
+	break;
+      } else if (pid == 0) {
+	// Child
+      } else {
+	// Parent
+      }
+    }
+  }
+  
 }
