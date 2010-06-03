cdba1f61aeaf3b4bacb4c104283aa3eb5be43fe1
tgummerer <t.gummerer@gmail.com>
2010-06-03 20:06:12 +0200

    Added license blocks

--- sighandlers.h
+++ sighandlers.h
@@ -1,3 +1,11 @@
+/**
+ * @name        : sighandlers.h
+ * @author      : Thomas Gummerer
+ * @version     : 0.1
+ * @description : The main program
+ * @copyright   : Copyright (c) Thomas Gummerer 2010 | All rights reserved.
+ * @license     : GPL
+ */
 #ifndef __SIGHANDLERS_H__
 #define __SIGHANDLERS_H__
 
