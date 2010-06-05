aceb2a252b1be0223af515604f95ee84fe3f70c8
tgummerer <t.gummerer@gmail.com>
2010-06-05 10:37:52 +0200

    * Removed description from header

--- child.h
+++ child.h
@@ -2,7 +2,6 @@
  * @name        : child.h
  * @author      : Thomas Gummerer
  * @version     : 0.1
- * @description : The main program
  * @copyright   : Copyright (c) Thomas Gummerer 2010 | All rights reserved.
  * @license     : GPL
  */
